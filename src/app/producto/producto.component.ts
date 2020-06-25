2681655504b546d64c8f5ca53ee34c593ca37222
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-25 11:54:50 +0200

    cant

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductosService } from '../servicios/productos.service';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
-  styleUrls: ['./producto.component.scss']
+  styleUrls: ['./producto.component.scss'],
+  host: {
+      "(click)": "closeOptions()"
+  }
 })
+
 export class ProductoComponent implements OnInit {
 
     _id: string;
@@ -14,6 +18,9 @@ export class ProductoComponent implements OnInit {
     picActive = 0;
 
     articulo: any;
+
+    @ViewChild('tallas', {static: false}) tallasRef: ElementRef;
+    @ViewChild('colores', {static: false}) coloresRef: ElementRef;
 
 
     constructor(private activatedRoute: ActivatedRoute,
@@ -25,7 +32,9 @@ export class ProductoComponent implements OnInit {
                                 .subscribe((res: any) => {
                                     this.producto = res.producto;
                                     this.articulo = {
-                                        talla: this.producto.tallas[0]
+                                        talla: this.producto.tallas[0],
+                                        color: this.producto.colores[0],
+                                        cantidad: 0
                                     }
                                 }, (error: any) => {
                                     console.log(error);
@@ -34,6 +43,36 @@ export class ProductoComponent implements OnInit {
 
     showPic(i) {
         this.picActive = i;
+    }
+
+    showTallas(event) {
+        event.stopPropagation();
+        this.tallasRef.nativeElement.classList.add('show');
+    }
+
+    setTalla(i) {
+        this.articulo.talla = this.producto.tallas[i];
+    }
+
+    showColores(event) {
+        event.stopPropagation();
+        this.coloresRef.nativeElement.classList.add('show');
+    }
+
+    setColor(i) {
+        this.articulo.color = this.producto.colores[i];
+    }
+
+    closeOptions() {
+        this.tallasRef.nativeElement.classList.remove('show');
+        this.coloresRef.nativeElement.classList.remove('show');
+    }
+
+    setCantidad(e) {
+        if (this.articulo.cantidad === 0 && e === -1) {
+            return;
+        }
+        this.articulo.cantidad +=e;
     }
 
 }
