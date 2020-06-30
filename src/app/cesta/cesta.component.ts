ae388e7ab8faf1f6512b62012d2737c2783b489b
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-30 12:48:44 +0200

    card 2

--- src/app/cesta/cesta.component.ts
+++ src/app/cesta/cesta.component.ts
@@ -23,11 +23,18 @@ export class CestaComponent implements OnInit {
         if(localStorage.getItem('articulos')){
             this.articulos = JSON.parse(localStorage.getItem('articulos'));
         }
+        this.pedido.total = 0;
         this.articulos.forEach(articulo => {
             articulo.importe = articulo.cantidad * articulo.precio;
             this.pedido.total += articulo.importe;
         })
         this.pedido.articulos = this.articulos;
+    }
+
+    deleteArticulo(i) {
+        this.articulos.splice(i, 1);
+        this.articulos = localStorage.setItem('articulos', JSON.stringify(this.articulos));
+        this.setPedido();
     }
 
 }
